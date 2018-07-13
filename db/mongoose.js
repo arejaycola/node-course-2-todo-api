39c41351a41c42701e5bbc6e8f76470a254be28c
Rob <robert.mastronicola.ctr@us.af.mil>
2018-07-13 10:32:03 -0400

    Set up app for heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://admin:admin123@ds229621.mlab.com:29621/todoapp', { useNewUrlParser: true } || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = {mongoose};
