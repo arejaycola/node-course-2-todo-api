d26f524f66c3814a3b0ab1fea5e2952617994094
Rob <robert.mastronicola.ctr@us.af.mil>
2018-07-13 09:39:04 -0400

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('../db/mongoose');
 const {Todo} = require('./models/todo');
@@ -24,10 +25,31 @@ app.post('/todos', (req, res) => {
 app.get('/todos', function(req, res){
 	Todo.find().then((todos) => {
 		res.send({todos: todos});
-	}, (e) => {
-		res.send(400).send(err);
+	}, (err) => {
+		res.status(400).send(err);
 	})
-})
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) =>{
+		if(!todo){
+			return res.status(404).send();
+		}
+
+		res.send({todo: todo});
+
+	}).catch((err) => {
+		res.status(400).send();
+	});
+});
+
+
 
 
 
