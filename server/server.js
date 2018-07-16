0992a6de2628c02fe5957263ff9befe83a5337cd
Rob <robert.mastronicola.ctr@us.af.mil>
2018-07-16 07:21:15 -0400

    Add Patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -67,6 +68,38 @@ app.delete('/todos/:id', (req, res) => {
 	}).catch((err) => {
 		return res.status(404).send();
 	});
+});
+
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	/* Only add the text and completed properties (if exist) to the body object */
+	var body = _.pick(req.body, ['text', 'completed']);
+
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	if(_.isBoolean(body.completed) && body.completed){
+		body.completedAt = new Date().getTime();
+	}else{
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) =>{
+		if(!todo){
+			return res.status(400).send();
+		}
+
+		res.status(400).send(todo);
+
+	}).catch((e) => {
+		res.status(400).send();
+	})
+
+
 });
 
 
