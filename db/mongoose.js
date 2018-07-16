7b984f68776223842be939330d11e1010762b821
Rob <robert.mastronicola.ctr@us.af.mil>
2018-07-16 08:59:23 -0400

    Setup seperate test and development envs

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://admin:admin123@ds229621.mlab.com:29621/todoapp', { useNewUrlParser: true } || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://admin:admin123@ds229621.mlab.com:29621/todoapp', { useNewUrlParser: true });
 // mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = {mongoose};
