d57c70776845879dc519cec465abee0134261f84
Rob <robert.mastronicola.ctr@us.af.mil>
2018-07-18 10:10:14 -0400

    Added POST /users/login route

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
+
 const {mongoose} = require('../db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
@@ -54,7 +55,6 @@ app.get('/todos/:id', (req, res) => {
 	});
 });
 
-
 app.delete('/todos/:id', (req, res) => {
 	var id = req.params.id;
 
@@ -72,7 +72,6 @@ app.delete('/todos/:id', (req, res) => {
 		return res.status(404).send();
 	});
 });
-
 
 app.patch('/todos/:id', (req, res) => {
 	var id = req.params.id;
@@ -125,6 +124,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate,  (req, res) => {
 	res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+
+	User.findByCredentials(body.email, body.password).then((user) => {
+		return user.generateAuthToken().then((token) => {
+			res.header('x-auth', token).send(user);
+		});
+	}).catch((e) => {
+		res.status(400).send();
+	});
 });
 
 
