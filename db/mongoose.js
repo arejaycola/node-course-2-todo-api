7c1c51f680c855246b4d165a6f1d98830be51805
Rob <robert.mastronicola.ctr@us.af.mil>
2018-08-06 09:19:09 -0400

    Added some async/await examples

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,6 +3,5 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
-// mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = {mongoose};
